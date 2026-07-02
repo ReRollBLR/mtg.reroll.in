e6d4ceb69ea7c44e43be5eb0066d981af116dcd1
Mohit <mgumber909@gmail.com>
2026-07-02 19:42:06 +0530

    announcement off

--- src/config/announcement.ts
+++ src/config/announcement.ts
@@ -3,7 +3,7 @@
  * Set `enabled: false` to hide it.
  */
 export const announcement = {
-  enabled: true,
+  enabled: false,
   text: "HAPPY PRIDE! June 27: We are doing a commander event to celebrate Pride month! ",
   ctaText: "Learn more",
   ctaHref: "https://magic.wizards.com/en/news/announcements/magic-presents-pride-returns-june-2026-with-a-new-promo-card",
