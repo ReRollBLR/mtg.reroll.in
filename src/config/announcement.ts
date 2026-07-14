63e89bae80f47326c432fb8c8f70d9d9c6b1dd19
Mohit <mgumber909@gmail.com>
2026-07-14 14:54:05 +0530

    fix: update ctaHref in announcement config to use relative path

--- src/config/announcement.ts
+++ src/config/announcement.ts
@@ -6,5 +6,5 @@ export const announcement = {
   enabled: true,
   text: "18th July : Join us a for a special sealed format with the new Marvel set. Open six packs, build a deck, and compete against players in a tournament",
   ctaText: "Sign up",
-  ctaHref: "mtg.reroll.in/tix",
+  ctaHref: "/tix",
 } as const;
